087b6e8a02e6ee3b560191670f2eebf9ab36bb18
RIZWAN VY <rizwanyoosaf37@gmail.com>
2024-05-16 20:48:01 +0530

    columns

--- 1.Data_Collection.py
+++ 1.Data_Collection.py
@@ -9,3 +9,13 @@ import pandas as pd
 base_url = "https://www.airlinequality.com/airline-reviews/british-airways"
 pages = 38
 page_size = 100
+
+# Columns :
+
+names = []
+locations = []
+date_review_posted = []
+rating = []
+review_title = []  
+detailed_review = []
+recommended = []
