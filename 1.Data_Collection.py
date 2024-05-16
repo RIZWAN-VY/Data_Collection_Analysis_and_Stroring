d36d1bca0654248ad2ddb0020b2c03e47534ad8e
RIZWAN VY <rizwanyoosaf37@gmail.com>
2024-05-16 20:49:19 +0530

    web scarping

--- 1.Data_Collection.py
+++ 1.Data_Collection.py
@@ -19,3 +19,19 @@ rating = []
 review_title = []  
 detailed_review = []
 recommended = []
+
+# WEB SCRAPING :
+
+for i in range(1, pages + 1):
+
+    print(f"Scraping page {i}")
+
+    # Create URL to collect links from paginated data
+    url = f"{base_url}/page/{i}/?sortby=post_date%3ADesc&pagesize={page_size}"
+
+    # Collect HTML data from this page
+    response = requests.get(url)
+
+    # Parse content
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
