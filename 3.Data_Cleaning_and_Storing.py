9ed279f40bc4317c7f299675438afda00d8408b1
RIZWAN VY <rizwanyoosaf37@gmail.com>
2024-05-19 20:56:10 +0530

    data cleaning and storing

---
+++ 3.Data_Cleaning_and_Storing.py
@@ -0,0 +1,11 @@
+"""
+    DATA CLEANING AND STORING
+======================================================================================================================================
+"""
+# Libraries
+import pandas as pd
+
+# # Loading the Data
+data = pd.read_csv('C:/Users/rizwa/Rizwan/Projects/Data_Collection_and_Storage/2.British_Airways_Customer_reviews.csv')
+print(data) 
+
